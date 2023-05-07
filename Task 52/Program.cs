10eaa1b84bd7dc6c0ff969c0fccdf8dfe5d0afc1
Leonid Solovev <ls3112@yandex.ru>
2023-05-07 14:51:59 +0300

    Task 52

--- Task 52/Program.cs
+++ Task 52/Program.cs
@@ -1,16 +1,18 @@
-﻿// Задача 47. Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
-// m = 3, n = 4.
-// 0,5 7 -2 -0,2
-// 1 -3,3 8 -9,9
-// 8 7,8 -7,1 9
+﻿// Задача 52. Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.
 
 int[,] array = NewArray(GetUserInput("Укажите размер массива по горизонтали: ", "Ошибка ввода!"), GetUserInput("Укажите размер массива по вертикали: ", "Ошибка ввода!"), GetUserInput("Укажите минимальное значение: ",
  "Ошибка ввода!"), GetUserInput("Укажите максимальное значение: ", "Ошибка ввода!"));
 
-
-Console.WriteLine($"m = {array.GetLength(0)}, n = {array.GetLength}");
-
 PrintArray(array);
+
+double [] averages = GetAvgForRows(array);
+
+PrintDoubleArray(averages);
 
 static int GetUserInput(string message, string errorMessage)
 {
@@ -37,7 +39,21 @@ static int[,] NewArray(int size1, int size2, int from, int to)
     return arr;
 }
 
-
+double [] GetAvgForRows(int [,] arr){
+    double [] avgs = new double [arr.GetLength(0)];
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            avgs[i] += arr[i,j];
+                   }
+    }
+    for (int k = 0; k < arr.GetLength(0); k++)
+    {
+        avgs[k] = avgs[k]/arr.GetLength(1);
+    }    
+    return avgs;
+}
 
 static void PrintArray(int[,] arr)
 {
@@ -48,7 +64,15 @@ static void PrintArray(int[,] arr)
         {
             Console.Write($"{arr[i, j]} ");
         }
-        Console.WriteLine();
+    Console.WriteLine();        
     }
 
+}
+
+ void PrintDoubleArray(double [] arr)
+{
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+                Console.Write($"{arr[i]} ");
+    }
 }
